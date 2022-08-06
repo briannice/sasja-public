0cb7ca15fbcb8ee12eff0e009e8be7753c5ea782
briannice <brian.nys.dev@gmail.com>
2022-08-06 13:46:53 +0200

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.tsx",
+    "./src/components/**/*.tsx",
+    "./src/layouts/**/*.tsx",
+    "./src/icons/**/*.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
