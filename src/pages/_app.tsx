27474afd0bcb935acb7e38be7dafcd9d2bef39b4
briannice <brian.nys.dev@gmail.com>
2022-08-30 16:17:22 +0200

    fix absolute root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
 
-import '@sasja-public/styles/main.css'
+import '@/styles/main.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
