228abde452f02baa2671cae7ae0fb34c0dcef58b
briannice <brian.nys.dev@gmail.com>
2022-08-30 16:55:56 +0200

    links and teams data

---
+++ src/data/links.ts
@@ -0,0 +1,24 @@
+import { youthLinks } from '@/data/teams'
+
+export const publicLinks = [
+  {
+    name: 'Kern',
+    href: '/kern',
+  },
+  {
+    name: 'Club',
+    href: '/club',
+  },
+  {
+    name: 'Jeugd',
+    sublinks: youthLinks,
+  },
+  {
+    name: 'Tickets',
+    href: '/#tickets-abbonementen',
+  },
+  {
+    name: 'Fanshop',
+    href: '/fanshop',
+  },
+]
---
+++ src/data/teams.ts
@@ -0,0 +1,59 @@
+export const teams = [
+  {
+    id: 'eerste-ploeg',
+    name: 'Eerste Ploeg',
+    youth: false,
+  },
+  {
+    id: 'beloften',
+    name: 'Beloften',
+    youth: false,
+  },
+  {
+    id: 'u18',
+    name: 'U18',
+    youth: true,
+  },
+  {
+    id: 'jm16',
+    name: 'JM16',
+    youth: true,
+  },
+  {
+    id: 'jm14',
+    name: 'JM14',
+    youth: true,
+  },
+  {
+    id: 'm13',
+    name: 'M13',
+    youth: true,
+  },
+  {
+    id: 'jm12',
+    name: 'JM12',
+    youth: true,
+  },
+  {
+    id: 'jm10',
+    name: 'JM10',
+    youth: true,
+  },
+  {
+    id: 'jm8',
+    name: 'JM8',
+    youth: true,
+  },
+]
+
+export const youthLinks = teams
+  .filter((team) => team.youth)
+  .map((team) => ({ name: team.name, href: `/teams/${team.id}` }))
+
+export const findTeamById = (id: string) => {
+  for (const team of teams) {
+    if (team.id === id) {
+      return team
+    }
+  }
+}
