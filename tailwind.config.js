931cf8ba594e8e5fc7b0b500976433ccbc87c4ee
briannice <brian.nys.dev@gmail.com>
2022-08-30 16:23:22 +0200

    tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,57 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.tsx",
-    "./src/components/**/*.tsx",
-    "./src/layouts/**/*.tsx",
-    "./src/icons/**/*.tsx",
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
+    './src/layouts/**/*.tsx',
+    './src/icons/**/*.tsx',
   ],
   theme: {
+    colors: {
+      transparent: colors.transparent,
+      white: colors.white,
+      light: colors.gray[100],
+      medium: colors.gray[300],
+      gray: colors.gray[500],
+      dark: colors.gray[700],
+      black: colors.gray[900],
+      primary: {
+        DEFAULT: '#EB2C2C',
+        dark: '#CC1313',
+      },
+      secondary: {
+        DEFAULT: '#11A149',
+        dark: '#0C7334',
+      },
+      tertiary: {
+        DEFAULT: '#B6A255',
+        dark: '#978541',
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2rem',
+        laptop: '3rem',
+        desktop: '4rem',
+      },
+      screens: {
+        laptop: '1024px',
+        desktop: '1280px',
+      },
+    },
+    fontFamily: {
+      kanit: "'Kanit', sans-serif",
+      lato: "'Lato', sans-serif",
+    },
+    screens: {
+      tablet: '640px',
+      laptop: '1024px',
+      desktop: '1280px',
+    },
     extend: {},
   },
   plugins: [],
-};
+}
