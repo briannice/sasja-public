ca6805f3dd343620e6aa33a3f7cc692e9ab49c62
briannice <brian.nys.dev@gmail.com>
2022-09-06 20:19:53 +0200

    ClubLogo component if request error

--- src/components/teams/ClubLogo.tsx
+++ src/components/teams/ClubLogo.tsx
@@ -7,18 +7,20 @@ type Props = {
 }
 
 export default function ClubLogo({ path, size }: Props) {
-  const [url, setUrl] = useState(
+  const [url, setUrl] = useState<string | null>(
     `https://admin.handballbelgium.be/lms_league_ws/public/img/${path}`
   )
   return (
     <figure className="relative aspect-square" style={{ width: size }}>
-      <Image
-        src={url}
-        alt="Club logo"
-        layout="fill"
-        objectFit="contain"
-        onError={() => setUrl('/pattern.png')}
-      />
+      {url && (
+        <Image
+          src={url}
+          alt="Club logo"
+          layout="fill"
+          objectFit="contain"
+          onError={() => setUrl(null)}
+        />
+      )}
     </figure>
   )
 }
