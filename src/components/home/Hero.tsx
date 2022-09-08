02659050ebe370f67f341b2c8f514032a02643ae
briannice <brian.nys.dev@gmail.com>
2022-09-08 19:31:22 +0200

    carousel components + event carousel item

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,3 +1,5 @@
+import Carousel from '@/components/carousel/Carousel'
+import EventCarouselItem from '@/components/carousel/items/EventCarouselItem'
 import { EventModel } from '@/types/models'
 import Image from 'next/image'
 import React from 'react'
@@ -6,13 +8,26 @@ type Props = {
   events: EventModel[]
 }
 
-export default function Hero({}: Props) {
+export default function Hero({ events }: Props) {
   return (
     <section className="grid grid-cols-3">
       <h2 className="sr-only">Hero</h2>
 
       <section className="col-span-2">
-        <h3>Evenementen</h3>
+        <h3 className="sr-only">Evenementen</h3>
+        <Carousel length={events.length} className="h-full">
+          {({ index, goNextItem, goPreviousItem }) =>
+            events.map((event, i) => (
+              <EventCarouselItem
+                key={event.id}
+                event={event}
+                goNextItem={goNextItem}
+                goPreviousItem={goPreviousItem}
+                show={index === i}
+              />
+            ))
+          }
+        </Carousel>
       </section>
 
       <section className="relative aspect-square">
