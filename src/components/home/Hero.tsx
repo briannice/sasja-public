3aaf05dbfe860e8edd43b4435ea8022556181aea
briannice <brian.nys.dev@gmail.com>
2022-09-08 18:05:04 +0200

    Hero next games layout

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,4 +1,5 @@
 import { EventModel } from '@/types/models'
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {
@@ -7,8 +8,21 @@ type Props = {
 
 export default function Hero({}: Props) {
   return (
-    <section>
-      <h2>Hero</h2>
+    <section className="grid grid-cols-3">
+      <h2 className="sr-only">Hero</h2>
+
+      <section className="col-span-2">
+        <h3>Evenementen</h3>
+      </section>
+
+      <section className="relative aspect-square">
+        <figure className="relative aspect-square">
+          <Image src="/handball-field.jpg" alt="Handball veld." layout="fill" objectFit="cover" />
+        </figure>
+        <div className="absolute inset-0 bg-black bg-opacity-75 p-8">
+          <h2 className="text-center text-4xl font-bold text-white">Volgende wedstrijden</h2>
+        </div>
+      </section>
     </section>
   )
 }
