a8e62ba557d9bc5faa758b5cd6b68d5fd7ab124a
briannice <brian.nys.dev@gmail.com>
2022-09-12 11:33:23 +0200

    Event card

--- src/components/home/News.tsx
+++ src/components/home/News.tsx
@@ -1,3 +1,4 @@
+import EventCard from '@/components/cards/EventCard'
 import NewsCard from '@/components/cards/NewsCard'
 import Container from '@/components/Container'
 import { EventModel, MatchReportModel, NewsModel, OpponentModel, TeamModel } from '@/types/models'
@@ -11,12 +12,15 @@ type Props = {
   matchReports: MatchReportModel[]
 }
 
-export default function News({ news }: Props) {
+export default function News({ news, events }: Props) {
   return (
     <Container className="grid grid-cols-3 gap-8">
       <h2 className="sr-only">News</h2>
       {news.map((news) => (
         <NewsCard key={news.id} news={news} />
+      ))}
+      {events.map((event) => (
+        <EventCard key={event.id} event={event} />
       ))}
     </Container>
   )
