397ae911ee6cf8c28f5baf4936f3db764d4607ab
briannice <brian.nys.dev@gmail.com>
2022-09-12 11:50:21 +0200

    extract carousel item logic in component

--- src/components/carousel/CarouselItem.tsx
+++ src/components/carousel/CarouselItem.tsx
@@ -1,14 +1,31 @@
+import useImage from '@/hooks/useImage'
 import { Transition } from '@headlessui/react'
 import clsx from 'clsx'
+import Image from 'next/image'
 import React, { ReactNode } from 'react'
+import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri'
 
 type Props = {
   children: ReactNode
+  col: string
+  goNextItem: () => void
+  goPreviousItem: () => void
+  id: string
   show: boolean
   className?: string | undefined
 }
 
-export default function CarouselItem({ children, show, className }: Props) {
+export default function CarouselItem({
+  children,
+  col,
+  goNextItem,
+  goPreviousItem,
+  id,
+  show,
+  className,
+}: Props) {
+  const image = useImage(col, id, 'lg')
+
   return (
     <Transition
       as="section"
@@ -21,7 +38,16 @@ export default function CarouselItem({ children, show, className }: Props) {
       leaveTo="opacity-0"
       className={clsx('absolute inset-0', className)}
     >
-      {children}
+      {image && <Image src={image} alt="Carousel image." layout="fill" objectFit="cover" />}
+      <section className="absolute inset-0 flex items-end space-x-16 bg-black bg-opacity-40 p-16">
+        <button onClick={() => goPreviousItem()}>
+          <RiArrowLeftSLine className="h-14 w-14 text-white transition-colors hover:text-primary" />
+        </button>
+        <div className="flex-1">{children}</div>
+        <button onClick={() => goNextItem()}>
+          <RiArrowRightSLine className="h-14 w-14 text-white transition-colors hover:text-primary" />
+        </button>
+      </section>
     </Transition>
   )
 }
--- src/components/carousel/items/EventCarouselItem.tsx
+++ src/components/carousel/items/EventCarouselItem.tsx
@@ -1,11 +1,9 @@
 import CarouselItem from '@/components/carousel/CarouselItem'
 import Link from '@/components/Link'
-import useImage from '@/hooks/useImage'
 import { EventModel } from '@/types/models'
 import { formatDate } from '@/utils/date'
-import Image from 'next/image'
 import React from 'react'
-import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri'
+import { RiArrowRightSLine } from 'react-icons/ri'
 
 type Props = {
   event: EventModel
@@ -15,40 +13,30 @@ type Props = {
 }
 
 export default function EventCarouselItem({ event, goNextItem, goPreviousItem, show }: Props) {
-  const url = useImage('events', event.id, 'lg')
   return (
-    <CarouselItem show={show}>
-      {url && <Image src={url} alt="Event image" layout="fill" objectFit="cover" />}
-      <section className="absolute inset-0 flex items-end space-x-16 bg-black bg-opacity-40 p-16">
-        <button onClick={() => goPreviousItem()}>
-          <RiArrowLeftSLine className="h-14 w-14 text-white transition-colors hover:text-primary" />
-        </button>
-        <div className="flex-1">
-          <h3 className="text-6xl font-bold text-white">{event.name}</h3>
-          <ul className="mt-8 flex divide-x divide-primary">
-            <li className="pr-8">
-              <p className="font-kanit text-xl text-white">{event.location}</p>
-            </li>
-            <li className="px-8">
-              <time className="font-kanit text-xl text-white">
-                {formatDate(event.time, 'DD/MM')}
-              </time>
-            </li>
-            <li className="pl-8">
-              <time className="font-kanit text-xl text-white">
-                {formatDate(event.time, 'HH:mm')}
-              </time>
-            </li>
-          </ul>
-          <Link href={`/evenement/${event.id}`} className="btn btn-primary btn-text-icon mt-8">
-            <span>Lees meer</span>
-            <RiArrowRightSLine />
-          </Link>
-        </div>
-        <button onClick={() => goNextItem()}>
-          <RiArrowRightSLine className="h-14 w-14 text-white transition-colors hover:text-primary" />
-        </button>
-      </section>
+    <CarouselItem
+      show={show}
+      col="events"
+      id={event.id}
+      goNextItem={goNextItem}
+      goPreviousItem={goPreviousItem}
+    >
+      <h3 className="text-6xl font-bold text-white">{event.name}</h3>
+      <ul className="mt-8 flex divide-x divide-primary">
+        <li className="pr-8">
+          <p className="font-kanit text-xl text-white">{event.location}</p>
+        </li>
+        <li className="px-8">
+          <time className="font-kanit text-xl text-white">{formatDate(event.time, 'DD/MM')}</time>
+        </li>
+        <li className="pl-8">
+          <time className="font-kanit text-xl text-white">{formatDate(event.time, 'HH:mm')}</time>
+        </li>
+      </ul>
+      <Link href={`/evenement/${event.id}`} className="btn btn-primary btn-text-icon mt-8">
+        <span>Lees meer</span>
+        <RiArrowRightSLine />
+      </Link>
     </CarouselItem>
   )
 }
