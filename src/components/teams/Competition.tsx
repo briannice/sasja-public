58c493c18f3642999cc421f081da453995e67a02
briannice <brian.nys.dev@gmail.com>
2022-09-14 16:17:50 +0200

    competition component responsive

--- src/components/teams/Competition.tsx
+++ src/components/teams/Competition.tsx
@@ -20,121 +20,127 @@ export default function Competition({ calendar, name, ranking, teamId }: Props) 
   }
 
   return (
-    <Container className="grid grid-cols-2 gap-8">
-      <h2 className="title1 col-span-2">{name}</h2>
-      <div className="flex flex-col items-center space-y-8">
-        <table>
-          <thead>
-            <tr>
-              <th>
-                <p>#</p>
-              </th>
-              <th>
-                <p>Team</p>
-              </th>
-              <th className="text-right">
-                <p>Ptn</p>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {ranking.map((rank) => (
-              <tr key={rank.id} className={clsx(rank.name === 'Sasja HC' && 'font-bold')}>
-                <td>
-                  <p>{rank.position}</p>
-                </td>
-                <td>
-                  <div className="flex items-center space-x-4">
-                    <ClubLogo path={rank.logo} size={20} />
-                    <p>{rank.short}</p>
-                  </div>
-                </td>
-                <td className="text-right">
-                  <p>{rank.points}</p>
-                </td>
+    <Container className="grid grid-cols-1 gap-8 desktop:grid-cols-2">
+      <h2 className="title1 col-span-1 desktop:col-span-2">{name}</h2>
+      <div className="space-y-8">
+        <div className="overflow-auto">
+          <table className="overflow-x-scroll">
+            <thead>
+              <tr>
+                <th>
+                  <p>#</p>
+                </th>
+                <th>
+                  <p>Team</p>
+                </th>
+                <th className="text-right">
+                  <p>Ptn</p>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <Link
-          href={`/team/${teamId}/klassement/${name.toLocaleLowerCase().replaceAll(/\ /g, '-')}`}
-          className="btn btn-primary btn-text-icon"
-        >
-          <span>Volledig klassement</span>
-          <RiArrowRightSLine />
-        </Link>
+            </thead>
+            <tbody>
+              {ranking.map((rank) => (
+                <tr key={rank.id} className={clsx(rank.name === 'Sasja HC' && 'font-bold')}>
+                  <td>
+                    <p>{rank.position}</p>
+                  </td>
+                  <td>
+                    <div className="flex items-center space-x-4">
+                      <ClubLogo path={rank.logo} size={20} />
+                      <p>{rank.short}</p>
+                    </div>
+                  </td>
+                  <td className="text-right">
+                    <p>{rank.points}</p>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="flex justify-center">
+          <Link
+            href={`/team/${teamId}/klassement/${name.toLocaleLowerCase().replaceAll(/\ /g, '-')}`}
+            className="btn btn-primary btn-text-icon"
+          >
+            <span>Volledig klassement</span>
+            <RiArrowRightSLine />
+          </Link>
+        </div>
       </div>
-      <div className="flex flex-col items-center space-y-8">
-        <table>
-          <thead>
-            <tr>
-              <th className="text-right">
-                <p>Thuis</p>
-              </th>
-              <th>
-                <p></p>
-              </th>
-              <th>
-                <p>Uit</p>
-              </th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {calendar.map((game) => (
-              <tr key={game.id}>
-                <td>
-                  <div className="flex items-center justify-end space-x-4">
-                    <p className="whitespace-nowrap">{game.home_short}</p>
-                    <ClubLogo path={game.home_logo} size={20} />
-                  </div>
-                </td>
-                <td>
-                  <p className="whitespace-nowrap text-center">
-                    {createScore(game.home_score, game.away_score)}
-                  </p>
-                </td>
-                <td>
-                  <div className="flex items-center space-x-4">
-                    <ClubLogo path={game.away_logo} size={20} />
-                    <p className="whitespace-nowrap">{game.away_short}</p>
-                  </div>
-                </td>
-                <td>
-                  {game.game_status_id === 2 && (
-                    <p
-                      className={clsx(
-                        'flex h-6 w-6 items-center justify-center rounded-full text-sm font-bold text-white',
-                        game.home_score === game.away_score
-                          ? 'bg-warning'
-                          : game.home_score > game.away_score && game.home_name === 'Sasja HC'
-                          ? 'bg-success'
-                          : game.home_score < game.away_score && game.away_name === 'Sasja HC'
-                          ? 'bg-success'
-                          : 'bg-error'
-                      )}
-                    >
-                      {game.home_score === game.away_score
-                        ? 'D'
-                        : game.home_score > game.away_score && game.home_name === 'Sasja HC'
-                        ? 'W'
-                        : game.home_score < game.away_score && game.away_name === 'Sasja HC'
-                        ? 'W'
-                        : 'L'}
-                    </p>
-                  )}
-                </td>
+      <div className="space-y-8">
+        <div className="overflow-auto">
+          <table>
+            <thead>
+              <tr>
+                <th className="text-right">
+                  <p>Thuis</p>
+                </th>
+                <th>
+                  <p></p>
+                </th>
+                <th>
+                  <p>Uit</p>
+                </th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <Link
-          href={`/team/${teamId}/kalender/${name.toLocaleLowerCase().replaceAll(/\ /g, '-')}`}
-          className="btn btn-primary btn-text-icon"
-        >
-          <span>Volledige kalender</span>
-          <RiArrowRightSLine />
-        </Link>
+            </thead>
+            <tbody>
+              {calendar.map((game) => (
+                <tr key={game.id}>
+                  <td>
+                    <div className="flex items-center justify-end space-x-4">
+                      <p>{game.home_short}</p>
+                      <ClubLogo path={game.home_logo} size={20} />
+                    </div>
+                  </td>
+                  <td>
+                    <p className="text-center">{createScore(game.home_score, game.away_score)}</p>
+                  </td>
+                  <td>
+                    <div className="flex items-center space-x-4">
+                      <ClubLogo path={game.away_logo} size={20} />
+                      <p>{game.away_short}</p>
+                    </div>
+                  </td>
+                  <td>
+                    {game.game_status_id === 2 && (
+                      <p
+                        className={clsx(
+                          'flex h-6 w-6 items-center justify-center rounded-full text-sm font-bold text-white',
+                          game.home_score === game.away_score
+                            ? 'bg-warning'
+                            : game.home_score > game.away_score && game.home_name === 'Sasja HC'
+                            ? 'bg-success'
+                            : game.home_score < game.away_score && game.away_name === 'Sasja HC'
+                            ? 'bg-success'
+                            : 'bg-error'
+                        )}
+                      >
+                        {game.home_score === game.away_score
+                          ? 'D'
+                          : game.home_score > game.away_score && game.home_name === 'Sasja HC'
+                          ? 'W'
+                          : game.home_score < game.away_score && game.away_name === 'Sasja HC'
+                          ? 'W'
+                          : 'L'}
+                      </p>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="flex justify-center">
+          <Link
+            href={`/team/${teamId}/kalender/${name.toLocaleLowerCase().replaceAll(/\ /g, '-')}`}
+            className="btn btn-primary btn-text-icon"
+          >
+            <span>Volledige kalender</span>
+            <RiArrowRightSLine />
+          </Link>
+        </div>
       </div>
     </Container>
   )
