c9d8b133ee567cdab9f19c5662990af149d4af33
briannice <brian.nys.dev@gmail.com>
2022-09-16 15:42:00 +0200

    create redirects for fb, insta and fanshop

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com', 'admin.handballbelgium.be'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/fanshop',
+        destination: 'https://teamfashion.be/nl/178-sasja-hc',
+        permanent: true,
+      },
+      { source: '/facebook', destination: 'https://www.facebook.com/SasjaHC', permanent: true },
+      { source: '/instagram', destination: 'https://www.instagram.com/sasjahc/', permanent: true },
+    ]
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
