55fc3bc3d717fbd5989e1b5aee86748ec65c2e98
briannice <brian.nys.dev@gmail.com>
2022-09-20 11:20:36 +0200

    youth plan

---
+++ src/pages/jeugd/jeugdbeleidsplan.tsx
@@ -0,0 +1,334 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Youthplan() {
+  return (
+    <main className="cms-content-wrapper">
+      <h1>SASJA Antwerpen Jeugdbeleidsplan 2022 - 2023</h1>
+      <div className="cms-content">
+        <h2>Jeugdwerking, mission statement</h2>
+        <p>De jeugdopleiding van SASJA Antwerpen is gefundeerd op vier pijlers :</p>
+        <ol>
+          <li>
+            <p>
+              De ontwikkeling van de speler op <b>fysiek</b> vlak
+            </p>
+          </li>
+          <li>
+            <p>
+              De ontwikkeling van de speler op <b>technisch</b> vlak
+            </p>
+          </li>
+          <li>
+            <p>
+              De ontwikkeling van de speler op <b>tactisch</b> vlak
+            </p>
+          </li>
+          <li>
+            <p>
+              Het bevorderen van het <b>speelplezier</b> en gezonde <b>competitiegeest</b> van de
+              speler
+            </p>
+          </li>
+        </ol>
+        <p>
+          Het is een opleiding die wordt gespreid over meerdere jaren, het volledige traject start
+          bij de bewegingsschool (vanaf 3 jaar) en de groep Multimove (vanaf 5 jaar) en eindigt bij
+          de U18.
+        </p>
+        <p>
+          Tijdens de opleiding zelf wordt de speler begeleid voor elk van de 4 pijlers volgens een
+          vaste methodiek in de vier fases van Maslow :
+        </p>
+        <ol>
+          <li>
+            <p>Onbewust niet kunnen</p>
+          </li>
+          <li>
+            <p>Bewust niet kunnen</p>
+          </li>
+          <li>
+            <p>Bewust kunnen</p>
+          </li>
+          <li>
+            <p>Onbewust kunnen</p>
+          </li>
+        </ol>
+        <figure>
+          <Image src="/jeugd/mazlow.jpg" alt="mazlow" layout="fill" objectFit="contain" />
+        </figure>
+        <p>
+          Begeleiden van de spelers vergt voor elke pijler en elke fase een weldoordacht plan van
+          aanpak door verscheidene actoren, binnen en buiten de club, die de speler ondersteunen:
+        </p>
+        <ul>
+          <li>
+            <p>Trainers</p>
+          </li>
+          <li>
+            <p>Jeugdcel</p>
+          </li>
+          <li>
+            <p>Oudercomité</p>
+          </li>
+          <li>
+            <p>Bestuur</p>
+          </li>
+          <li>
+            <p>Ouders</p>
+          </li>
+          <li>
+            <p>Supporters/sympathisanten</p>
+          </li>
+          <li>
+            <p>Scheidsrechters</p>
+          </li>
+        </ul>
+        <h2>Jeugdwerking, een visie op de toekomst</h2>
+        <p>
+          SASJA Antwerpen wil een jeugdwerking uitgaande van een dubbele opleidingspiramide. Een
+          dubbele piramide waar in de basis alle jongeren die handbal wensen te spelen, kunnen
+          instappen en naar enerzijds eigen keuze en anderzijds talentdetectie, kunnen doorgroeien
+          naar Tophandbal of recreatief handbal.
+        </p>
+        <figure>
+          <Image src="/jeugd/piramide.jpg" alt="mazlow" layout="fill" objectFit="contain" />
+        </figure>
+        <h2>Jeugdwerking, meer dan spelen alleen</h2>
+        <p>
+          Een volledige sportbeleving bestaat uit meer dan enkel het spelen op zich. Hiervoor biedt
+          Sasja Antwerpen naast handbalopleiding, nog andere uitdagingen aan onze jeugdspelers die
+          onontbeerlijk zijn voor totaal concept binnen het handbal.
+        </p>
+        <ul>
+          <li>
+            <p>Scheidsrechters</p>
+          </li>
+          <li>
+            <p>Trainers</p>
+          </li>
+          <li>
+            <p>Bestuurders</p>
+          </li>
+        </ul>
+        <p>
+          Het aanbieden van een zo volledig mogelijk gamma aan mogelijkheden binnen de club, heeft
+          twee concrete doelstellingen :
+        </p>
+        <ol>
+          <li>
+            <p>Doorstromen van jeugdspelers naar het Fanionteam van de club door</p>
+            <ul>
+              <li>
+                <p>Opleiding</p>
+              </li>
+              <ul>
+                <li>
+                  <p>Aanleren van tactisch inzicht</p>
+                </li>
+                <li>
+                  <p>Verfijnen van technische vaardigheden</p>
+                </li>
+                <li>
+                  <p>Stimuleren van fysieke vaardigheden</p>
+                </li>
+              </ul>
+              <li>
+                <p>Uitdaging</p>
+              </li>
+              <li>
+                <p>Talentdetectie en talentbegeleiding</p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p>Binden van alle spelers aan de club voor, tijdens en na het handballen </p>
+            <ul>
+              <li>
+                <p>Creëren van een totaalbeleving</p>
+              </li>
+              <ul>
+                <li>
+                  <p>Groepsgevoel bevorderen</p>
+                </li>
+                <li>
+                  <p>Spelvreugde benadrukken</p>
+                </li>
+              </ul>
+              <li>
+                <p>De club is meer dan de eigen ploeg</p>
+              </li>
+              <li>
+                <p>De club is meer dan trainen en competitie</p>
+              </li>
+            </ul>
+          </li>
+        </ol>
+        <h2>Elke doelstelling zijn actor, elke actor zijn doelstelling</h2>
+        <p>
+          Om de mission statement uit voeren in de visie op de toekomst dient de jeugdwerking terug
+          te vallen op verscheidene actoren.
+        </p>
+        <p>
+          Elke actor krijgt zijn doelstelling en missie, en elke doelstelling krijgt een of meerdere
+          actoren gebaseerd op de pijlers.
+        </p>
+        <p>
+          De <b>trainers en coaches</b> staan in voor de ontwikkeling van de spelers op fysiek,
+          technisch en conditioneel vlak. Drie pijlers voor de speltechnische ontwikkeling van de
+          speler. Ze volgen hiervoor het jeugdopleidingsplan, waar per categorie de
+          opleidingsdoelstellingen zijn uitgeschreven.
+        </p>
+        <p>
+          Ze gebruiken dit opleidingsplan om de trainingen voor te bereiden, te geven en op elkaar
+          af te stemmen.
+        </p>
+        <p>Het opleidingsplan is een addendum aan dit jeugdbeleidsplan.</p>
+        <p>
+          Het opleidingsplan wordt jaarlijks bekeken door zowel de Technisch als de Administratief
+          Jeugdcoördinator, up-to-date gebracht en besproken met de jeugdtrainers.
+        </p>
+        <p>
+          De <b>jeugdcel</b> bestaat uit de jeugdcoördinatoren, promotieverantwoordelijke,
+          vrijwilligersverantwoordelijke en ploegbegeleiders. De jeugdcel ondersteunt de trainers en
+          coaches op extra-sportief vlak en is verantwoordelijk voor de omkadering waarin de
+          opleiding plaatsvindt. De leden van de jeugdcel zorgen ervoor dat de trainers en coaches
+          hun focus volledig gericht kan blijven op de sporttechnische aspecten van de opleiding. Ze
+          doen dit concreet door :
+        </p>
+        <ul>
+          <li>
+            <p>Het maximaal beperken van de administratieve taken van de trainers en coaches.</p>
+          </li>
+          <li>
+            <p>Het verzorgen van de communicatie naar</p>
+            <ul>
+              <li>
+                <p>Ouders</p>
+              </li>
+              <li>
+                <p>Spelers</p>
+              </li>
+              <li>
+                <p>Trainers</p>
+              </li>
+              <li>
+                <p>Bestuur</p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p>Organiseren van een aantal zaken inherent aan de competitie</p>
+            <ul>
+              <li>
+                <p>Zorgen voor de aanwezigheid van tafelofficials</p>
+              </li>
+              <li>
+                <p>Zorgen voor de aanwezigheid van (jeugd)scheidsrechters</p>
+              </li>
+              <li>
+                <p>Reservatie van de zalen</p>
+              </li>
+              <li>
+                <p>Coördineren van het transport bij wedstrijden op verplaatsing</p>
+              </li>
+              <li>
+                <p>Coördineren van de speelkalender</p>
+              </li>
+              <li>
+                <p>Begeleiden van de jeugdscheidsrechters</p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p>Organiseren van de trainingen</p>
+            <ul>
+              <li>
+                <p>Reservatie van de zalen in de verschillende sporthallen</p>
+              </li>
+              <li>
+                <p>Zorgen voor geschikte en (bij voorkeur) gediplomeerde jeugdtrainers</p>
+              </li>
+              <li>
+                <p>Stimuleren van (nog) niet-gediplomeerde trainers om cursus te volgen</p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p>Zorgen voor het aansluiten van nieuwe spelers</p>
+          </li>
+          <li>
+            <p>Stimuleren van jeugdspelers (U16) om de cursus Jeugdscheidsrechter te volgen</p>
+          </li>
+          <li>
+            <p>
+              Ondersteunen van de trainers bij het organiseren van een deelname aan een binnen- of
+              buitenlands jeugdtornooi
+            </p>
+          </li>
+          <li>
+            <p>Uitschrijven en indienen van subsidiedossiers</p>
+          </li>
+        </ul>
+        <p>
+          De jeugdcel kan terugvallen op het <b>bestuur</b> van de club, zowel voor het beheer van
+          de financiën, als voor de zaken die wettelijk en statutair onder haar bevoegdheid vallen.
+          Het bestuur draagt er zorg voor dat de ingediende subsidiedossiers juridisch juist worden
+          ingediend en zijn verantwoordelijk voor het juiste gebruik van de verworven subsidies.
+        </p>
+        <p>
+          In samenspraak met de jeugdcel ziet het bestuur erop toe dat de aangevraagde zalen,
+          materialen, e.d. door de externe partners op correcte wijze ter beschikking worden
+          gesteld.
+        </p>
+        <p>
+          Het bestuur is ook verantwoordelijk voor de synergie binnen de club, het draagt er zorg
+          voor dat alle actoren binnen de club, zich lid voelen van dezelfde club.
+        </p>
+        <p>
+          Het oudercomité bestaat uit alle ouders en sympathisanten van de (jeugd)spelers. Het
+          ondersteunt de jeugdcel in zijn werking. Het hoofddoel van het oudercomité is de
+          empowerment van de ouders bij de club. Bekwame betrokkenheid van de ouders en in extenso
+          de spelers ontstaat wanneer bestuur of jeugdcel taken kan toebedelen
+        </p>
+        <ul>
+          <li>
+            <p>Tafelofficial</p>
+          </li>
+          <li>
+            <p>Wasouders</p>
+          </li>
+          <li>
+            <p>
+              Organisatie van kleine niet-sportieve evenementen, waarvan de opbrengst kan besteed
+              worden aan de aankoop van materiaal of aangewend bij de deelname aan een buitenlands
+              tornooi
+            </p>
+            <ul>
+              <li>
+                <p>Marsepeinverkoop</p>
+              </li>
+              <li>
+                <p>Fuif</p>
+              </li>
+              <li>
+                <p>Car Wash</p>
+              </li>
+              <li>
+                <p>...</p>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <p>
+          Pas wanneer de betrokkenheid van alle actoren volledig is, wordt de club meer dan een
+          sportbeleving alleen. Dan zal elke actor voor, tijdens en na het handballen betrokken
+          blijven bij de club.
+        </p>
+        <figure>
+          <Image src="/jeugd/club.jpg" alt="mazlow" layout="fill" objectFit="contain" />
+        </figure>
+      </div>
+    </main>
+  )
+}
