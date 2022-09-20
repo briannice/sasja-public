54984e617bf943c1d89e1711e0eb32f00e773303
briannice <brian.nys.dev@gmail.com>
2022-09-20 15:42:12 +0200

    page titles for club pages

--- src/pages/club/aanspreekpunt-integriteit.tsx
+++ src/pages/club/aanspreekpunt-integriteit.tsx
@@ -1,58 +1,65 @@
+import Head from 'next/head'
 import React from 'react'
 
 export default function ContactPointIntegrity() {
   return (
-    <main className="cms-content-wrapper">
-      <h1>Aanspreekpunt Integriteit (API)</h1>
-      <div className="cms-content">
-        <h2>Wat?</h2>
-        <p>
-          Een aanspreekpersoon integriteit is een laagdrempelig aanspreekpunt waarbij sporters, hun
-          ouders, begeleiders of andere betrokkenen terechtkunnen met een vraag, vermoeden of klacht
-          over (seksueel) grensoverschrijdend gedrag of lichamelijke en seksuele integriteit. Een
-          Aanspreekpersoon Integriteit (API) draagt er mee zorg voor dat het beleid voldoende
-          aandacht geeft aan preventie van en omgaan met (seksueel) grensoverschrijdend gedrag.
-        </p>
-        <h2>Waarom?</h2>
-        <p>
-          Omdat het de verantwoordelijkheid is van de sportorganisatie om te waken over de
-          bescherming van sporters die aan hen worden toevertrouwd. Ervaringen van sporters met
-          (seksueel) grensoverschrijdend gedrag in een sportcontext maken duidelijk dat het geen
-          marginaal probleem is, en dat nog veel incidenten niet tijdig worden opgemerkt en
-          aangepakt. Een duidelijk, eerste aanspreekpunt moet onderdeel uitmaken van een beleid om
-          meer verantwoordelijkheid te nemen voor die veiligheid.
-        </p>
-        <h2>Wie en op welk niveau?</h2>
-        <p>
-          Een Aanspreekpunt is belangrijk op het niveau van de sporters zelf, dus dichtbij en
-          zichtbaar. Een CLUB-API is een persoon, verkozen of aangesteld door het bestuur, die de
-          nodige competenties heeft en opgeleid is om binnen de lokale sportorganisatie een eerste
-          aanspreekpunt rond lichamelijke en seksuele integriteit te zijn. Een FEDERATIE-API
-          ondersteunt de Club-API en helpt en adviseert deze om zijn rol uit te bouwen, en neemt ook
-          zelf initiatief.
-        </p>
-        <h2>Concreet</h2>
-        <p>
-          Indien je met een probleem zit, neem je best eerst contact op met onze club API. Bij Sasja
-          is dat Kenny Claes. Hij is aangesteld door het bestuur en is opgeleid om binnen de club
-          een eerste aanspreekpunt rond lichamelijke en seksuele integriteit te zijn.
-        </p>
-        <p>
-          Je kan Kenny aanspreken in de sportzaal of je kan hem een berichtje sturen op{' '}
-          <a href="mailto:api@sasja-antwerpen.be">api@sasja-antwerpen.be</a>. Dit email adres is
-          vertrouwelijk en voor niemand anders toegankelijk.
-        </p>
-        <p>
-          Indien je liever niet met Kenny spreekt, om welke reden dan ook, dan mag je ook direct
-          contact opnemen met de Federatie-API. Dat is Sabine Stieglitz en ze is bereikbaar op{' '}
-          <a href="mailto:api@sasja-antwerpen.be">api.handbal@gmail.com</a> of 0472/95 79 58.
-        </p>
-        <p>
-          Voor dringende gevallen die onmiddellijke hulpverlening vereisen, contacteert u nog altijd
-          de hulpdiensten op 101 en 112. Verder kan u ook terecht op het nummer 1712, de hulplijn
-          voor geweld, misbruik en kindermishandeling, op werkdagen van 9u00 tot 17u00.
-        </p>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Sasja HC | Aanspreekpunt integriteit</title>
+      </Head>
+      <main className="cms-content-wrapper">
+        <h1>Aanspreekpunt Integriteit (API)</h1>
+        <div className="cms-content">
+          <h2>Wat?</h2>
+          <p>
+            Een aanspreekpersoon integriteit is een laagdrempelig aanspreekpunt waarbij sporters,
+            hun ouders, begeleiders of andere betrokkenen terechtkunnen met een vraag, vermoeden of
+            klacht over (seksueel) grensoverschrijdend gedrag of lichamelijke en seksuele
+            integriteit. Een Aanspreekpersoon Integriteit (API) draagt er mee zorg voor dat het
+            beleid voldoende aandacht geeft aan preventie van en omgaan met (seksueel)
+            grensoverschrijdend gedrag.
+          </p>
+          <h2>Waarom?</h2>
+          <p>
+            Omdat het de verantwoordelijkheid is van de sportorganisatie om te waken over de
+            bescherming van sporters die aan hen worden toevertrouwd. Ervaringen van sporters met
+            (seksueel) grensoverschrijdend gedrag in een sportcontext maken duidelijk dat het geen
+            marginaal probleem is, en dat nog veel incidenten niet tijdig worden opgemerkt en
+            aangepakt. Een duidelijk, eerste aanspreekpunt moet onderdeel uitmaken van een beleid om
+            meer verantwoordelijkheid te nemen voor die veiligheid.
+          </p>
+          <h2>Wie en op welk niveau?</h2>
+          <p>
+            Een Aanspreekpunt is belangrijk op het niveau van de sporters zelf, dus dichtbij en
+            zichtbaar. Een CLUB-API is een persoon, verkozen of aangesteld door het bestuur, die de
+            nodige competenties heeft en opgeleid is om binnen de lokale sportorganisatie een eerste
+            aanspreekpunt rond lichamelijke en seksuele integriteit te zijn. Een FEDERATIE-API
+            ondersteunt de Club-API en helpt en adviseert deze om zijn rol uit te bouwen, en neemt
+            ook zelf initiatief.
+          </p>
+          <h2>Concreet</h2>
+          <p>
+            Indien je met een probleem zit, neem je best eerst contact op met onze club API. Bij
+            Sasja is dat Kenny Claes. Hij is aangesteld door het bestuur en is opgeleid om binnen de
+            club een eerste aanspreekpunt rond lichamelijke en seksuele integriteit te zijn.
+          </p>
+          <p>
+            Je kan Kenny aanspreken in de sportzaal of je kan hem een berichtje sturen op{' '}
+            <a href="mailto:api@sasja-antwerpen.be">api@sasja-antwerpen.be</a>. Dit email adres is
+            vertrouwelijk en voor niemand anders toegankelijk.
+          </p>
+          <p>
+            Indien je liever niet met Kenny spreekt, om welke reden dan ook, dan mag je ook direct
+            contact opnemen met de Federatie-API. Dat is Sabine Stieglitz en ze is bereikbaar op{' '}
+            <a href="mailto:api@sasja-antwerpen.be">api.handbal@gmail.com</a> of 0472/95 79 58.
+          </p>
+          <p>
+            Voor dringende gevallen die onmiddellijke hulpverlening vereisen, contacteert u nog
+            altijd de hulpdiensten op 101 en 112. Verder kan u ook terecht op het nummer 1712, de
+            hulplijn voor geweld, misbruik en kindermishandeling, op werkdagen van 9u00 tot 17u00.
+          </p>
+        </div>
+      </main>
+    </>
   )
 }
