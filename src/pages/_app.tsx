e8982bc8c984ef9cec5c1df6e079a1bdd4333ff0
briannice <brian.nys.dev@gmail.com>
2022-09-26 15:42:44 +0200

    google analytics custom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
 import Layout from '@/components/layout/Layout'
 import { AppProps } from 'next/app'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import '@/styles/main.css'
+import { useRouter } from 'next/router'
+
+const pageview = (url: string) => {
+  window.gtag('config', process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, {
+    page_path: url,
+  })
+}
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <Layout>
       <GoogleAnalytics trackPageViews />
