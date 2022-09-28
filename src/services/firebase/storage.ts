320e79c7bb4c9ecaa9577290cb138dd1d933fc8e
briannice <brian.nys.dev@gmail.com>
2022-09-28 17:21:31 +0200

    default image for event and nieuws detail page

--- src/services/firebase/storage.ts
+++ src/services/firebase/storage.ts
@@ -13,3 +13,13 @@ export const downloadImage = async (path: string, size: 'sm' | 'lg') => {
     return null
   }
 }
+
+export const downloadDefaultEventImage = async () => {
+  try {
+    const storageRef = ref(storage, '/defaults/event_1280x1280.jpg')
+    const url = await getDownloadURL(storageRef)
+    return url
+  } catch (error) {
+    return null
+  }
+}
