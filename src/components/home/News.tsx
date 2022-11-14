f747b96955995bdc8c433d4c0de176351aed0d50
briannice <brian.nys.dev@gmail.com>
2022-11-14 10:14:16 +0100

    remove disco event

--- src/components/home/News.tsx
+++ src/components/home/News.tsx
@@ -3,7 +3,6 @@ import MatchreportCard from '@/components/cards/MatchreportCard'
 import NewsCard from '@/components/cards/NewsCard'
 import Container from '@/components/Container'
 import { TimeLine } from '@/types/models'
-import Image from 'next/image'
 import React from 'react'
 
 type Props = {
@@ -14,12 +13,9 @@ export default function News({ timeLine }: Props) {
   return (
     <Container className="grid grid-cols-4 gap-8 tablet:grid-cols-8 laptop:grid-cols-12">
       <h2 className="sr-only">News</h2>
-      <section className="card card-col-1">
-        <Image src="/disco.jpeg" alt="Disco" layout="fill" objectFit="contain" />
-      </section>
       {timeLine.map(
         ({ data, name }, i) =>
-          i < 5 &&
+          i < 6 &&
           (name === 'event' ? (
             <EventCard key={data.id} event={data} />
           ) : name === 'matchreport' ? (
