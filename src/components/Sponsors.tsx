26cc7617c2941f1ba75a2166355687014884edf5
briannice <brian.nys.dev@gmail.com>
2023-09-18 19:55:06 +0200

    Update sponsors

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -2,13 +2,23 @@ import clsx from 'clsx'
 import Image from 'next/image'
 import React from 'react'
 
-export const sponsors = [
+type Sponsor = {
+  name: string
+  path: string
+  href: string
+  aspect: string
+}
+
+export const mainSponsors: Sponsor[] = [
   {
-    name: 'dca',
-    path: '/sponsors/dca.png',
-    href: 'https://www.dca.be/nl',
-    aspect: 'aspect-[834/400]',
+    name: 'filter technics',
+    path: '/sponsors/filter-technics.svg',
+    href: 'https://www.filter-technics.be/nl',
+    aspect: 'aspect-[335/88]',
   },
+]
+
+export const secondarySponsors: Sponsor[] = [
   {
     name: 'umicore',
     path: '/sponsors/umicore.png',
@@ -20,12 +30,6 @@ export const sponsors = [
     path: '/sponsors/kempa.png',
     href: 'https://en.kempa-sports.com/en/start',
     aspect: 'aspect-[1772/648]',
-  },
-  {
-    name: 'filter technics',
-    path: '/sponsors/filter-technics.svg',
-    href: 'https://www.filter-technics.be/nl',
-    aspect: 'aspect-[335/88]',
   },
   {
     name: 'de waele',
@@ -61,20 +65,37 @@ export const sponsors = [
 
 export default function Sponsors() {
   return (
-    <div className="flex flex-wrap justify-center">
-      {sponsors.map((sponsor) => (
-        <a
-          key={sponsor.name}
-          href={sponsor.href}
-          target="_blank"
-          rel="noreferrer"
-          className="mx-8 my-4 laptop:mx-12 laptop:my-6"
-        >
-          <figure className={clsx('relative h-8 tablet:h-10 laptop:h-12', sponsor.aspect)}>
-            <Image src={sponsor.path} alt="Sponsor image" layout="fill" objectFit="cover" />
-          </figure>
-        </a>
-      ))}
+    <div>
+      <div className="flex flex-wrap justify-center">
+        {mainSponsors.map((sponsor) => (
+          <a
+            key={sponsor.name}
+            href={sponsor.href}
+            target="_blank"
+            rel="noreferrer"
+            className="mx-8 my-4 laptop:mx-12 laptop:my-6"
+          >
+            <figure className={clsx('relative h-12 tablet:h-16 laptop:h-20', sponsor.aspect)}>
+              <Image src={sponsor.path} alt="Sponsor image" layout="fill" objectFit="cover" />
+            </figure>
+          </a>
+        ))}
+      </div>
+      <div className="flex flex-wrap justify-center">
+        {secondarySponsors.map((sponsor) => (
+          <a
+            key={sponsor.name}
+            href={sponsor.href}
+            target="_blank"
+            rel="noreferrer"
+            className="mx-8 my-4 laptop:mx-12 laptop:my-6"
+          >
+            <figure className={clsx('relative h-8 tablet:h-10 laptop:h-12', sponsor.aspect)}>
+              <Image src={sponsor.path} alt="Sponsor image" layout="fill" objectFit="cover" />
+            </figure>
+          </a>
+        ))}
+      </div>
     </div>
   )
 }
