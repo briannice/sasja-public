5a73838da4aa7c67791bd973bcfdcdd4f9901e85
Kenny Claes <kenny.claes@benevolent.ai>
2023-12-04 16:58:07 +0100

    fix: recreanten pagina

--- src/pages/recreanten/index.tsx
+++ src/pages/recreanten/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Container from "@/components/Container";
 import Link from "@/components/Link";
 import {RiExternalLinkLine} from "react-icons/ri";
+import Image from "next/image";
 
 const trainings = [
     {
@@ -26,6 +27,11 @@ export default function index() {
             </Head>
             <main>
                 <h1 className="title1 mt-8">Recreanten</h1>
+                <div className="cms-content">
+                <figure>
+                    <Image src="/recreanten/team.jpg" layout="fill" objectFit="contain" />
+                </figure>
+                </div>
                 <h2 className="title1 mt-8">Trainingen</h2>
                 <Container className="space-y-8">
                     {trainings.map(({data, day}) => (
@@ -64,41 +70,15 @@ export default function index() {
                 </Container>
                 <h2 className="title1 mt-8">Info</h2>
                 <Container card={true} className="card-text">
-                    <div className="cms-content">
                         <h2>Ontdek de Passie van Handbal met Onze Recreantenploeg!</h2>
                         <p>Ben jij op zoek naar een leuke en energieke manier om actief te blijven en nieuwe vrienden te
-                            maken? Zoek niet verder! Onze recreantenploeg heet jou van harte welkom om de opwinding en
-                            dynamiek van deze geweldige sport te ervaren.</p>
-                        <h2>Waarom Kiezen voor Onze Recreantenploeg?</h2>
-                        <h3>Gezellige sfeer</h3>
-                        <p>We geloven in het creëren van een omgeving waarin iedereen zich welkom voelt. Ongeacht je
-                            ervaring of vaardigheidsniveau, je zult merken dat onze ploeg een hechte groep is die
-                            samenkomt om plezier te hebben.</p>
-                        <h3>Beweeg en blijf fit</h3>
-                        <p>Handbal is niet alleen leuk, maar ook een fantastische manier om in vorm te blijven. Je zult
-                            genieten van de mix van cardio, kracht en behendigheid die handbal biedt, terwijl je de
-                            alledaagse stress vergeet.</p>
-                        <h3>Leer nieuwe vaardigheden</h3>
-                        <p>Of je nu een doorgewinterde handbalspeler bent of nog nooit een bal hebt aangeraakt, onze
-                            ervaren coach staat klaar om je te helpen. Leer de basisprincipes of verfijn je techniek –
-                            er is altijd ruimte om te groeien.</p>
-                        <h3>Wedstrijdplezier</h3>
-                        <p>Als je van een vleugje competitie houdt, zul je dol zijn op onze recreantenwedstrijden. Samen
-                            met gelijkgestemde teamgenoten krijg je de kans om je vaardigheden in de praktijk te brengen
-                            tijdens vriendschappelijke wedstrijden en toernooien.</p>
-                        <h3>Sociale evenementen</h3>
-                        <p>Onze ploeg is meer dan alleen handbal. We organiseren binnen de club regelmatig sociale
-                            evenementen waardoor je de kans krijgt om je medespelers buiten het veld te leren
-                            kennen.</p>
+                            maken? </p>
+                    <p>Zoek niet verder! Onze recreantenploeg heet jou van harte welkom!</p>
                         <h2>Doe Vandaag Nog Mee!</h2>
                         <p>Of je nu een oude handbalprof bent of gewoon nieuwsgierig bent naar deze geweldige sport,
-                            onze recreantenploeg staat open voor iedereen die wil deelnemen. Meld je aan en ontdek het
-                            plezier, de vriendschap en de gezondheidsvoordelen van handbal.</p>
-                        <p>Neem <a href="mailto:recreanten@sasja-antwerpen.be">contact</a> met ons op en sluit je aan
-                            bij onze enthousiaste
-                            recreantenploeg of kom een bezoekje brengen op onze training.</p>
+                            onze recreantenploeg staat open voor iedereen die wil deelnemen: <a href="mailto:recreanten@sasja-antwerpen.be">meld je aan</a> en ontdek de sfeer, het
+                            plezier en de vriendschap binnen het handbal.</p>
                         <p>Laten we samen plezier hebben, bewegen en de magie van handbal beleven! Tot gauw!️</p>
-                    </div>
                 </Container>
             </main>
         </>
