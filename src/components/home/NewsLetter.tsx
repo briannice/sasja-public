0ccab46a2b04a317f44356eec05806743d8f7a39
briannice <brian.nys.dev@gmail.com>
2024-01-24 17:54:35 +0100

    Make link new tab

--- src/components/home/NewsLetter.tsx
+++ src/components/home/NewsLetter.tsx
@@ -6,7 +6,7 @@ export default function NewsLetter() {
     <section className="mt-8 bg-white py-8">
       <div className="container flex flex-col items-center space-y-4">
         <h2 className="title1">Nieuwsbrief</h2>
-        <Link href="/nieuwsbrief" className="btn btn-text-icon btn-primary">
+        <Link blank href="/nieuwsbrief" className="btn btn-text-icon btn-primary">
           <span>Inschrijven voor de nieuwsbrief</span>
         </Link>
       </div>
