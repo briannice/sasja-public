c320c2a9ac6b3c670e2b617191c4abd1700325fd
kclaes <kenny.claes@gmail.com>
2024-04-03 13:37:35 +0200

    fix: update federation api information

--- src/pages/club/aanspreekpunt-integriteit.tsx
+++ src/pages/club/aanspreekpunt-integriteit.tsx
@@ -50,8 +50,8 @@ export default function ContactPointIntegrity() {
           </p>
           <p>
             Indien je liever niet met Kenny spreekt, om welke reden dan ook, dan mag je ook direct
-            contact opnemen met de Federatie-API's. Dat zijn Heidi Frans en Janneke Van Megen en ze
-            zijn bereikbaar op{' '}
+            contact opnemen met de Federatie-API&apos;s. Dat zijn Heidi Frans en Janneke Van Megen
+            en ze zijn bereikbaar op{' '}
             <a href="mailto:api@handbal.be">api@handbal.be</a> of 0494/845161 (Heidi).
           </p>
           <p>
