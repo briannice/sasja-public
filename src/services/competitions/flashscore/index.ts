4021cb710791b0140b3a4d6ccd1d76328ad1b6f7
kclaes <kenny.claes@gmail.com>
2024-10-02 10:47:15 +0200

    fix: puppeteer on vercel... (?)

--- src/services/competitions/flashscore/index.ts
+++ src/services/competitions/flashscore/index.ts
@@ -1,4 +1,6 @@
 import { TeamCompetition } from '@/types/models'
+import puppeteer from 'puppeteer-core'
+import chromium from '@sparticuz/chromium-min'
 
 export type FlashScoreGame = {
   id: string
@@ -9,7 +11,36 @@ export type FlashScoreGame = {
   awayScore: number,
 }
 
+const localExecutablePath =
+  process.platform === 'win32'
+    ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+    : process.platform === 'linux'
+      ? '/usr/bin/google-chrome'
+      : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+const remoteExecutablePath =
+  'https://github.com/Sparticuz/chromium/releases/download/v127.0.0/chromium-v127.0.0-pack.tar'
+
+const puppeteerLocal = process.env.PUPPETEER_LOCATION === 'LOCAL'
+
+
 export class FlashScoreService {
+  private async getBrowser() {
+    return await puppeteer.launch({
+      ignoreDefaultArgs: ['--enable-automation'],
+      args: puppeteerLocal
+        ? [
+          '--disable-blink-features=AutomationControlled',
+          '--disable-features=site-per-process',
+          '-disable-site-isolation-trials',
+        ]
+        : [...chromium.args, '--disable-blink-features=AutomationControlled'],
+      executablePath: puppeteerLocal
+        ? localExecutablePath
+        : await chromium.executablePath(remoteExecutablePath),
+      headless: chromium.headless,
+    })
+  }
+
   private getLeague(competition: TeamCompetition) {
     if (competition.name === 'Super Handball League')
       return 'super-handball-league'
@@ -17,45 +48,31 @@ export class FlashScoreService {
   }
 
   public async getGames(competition: TeamCompetition, page: string): Promise<FlashScoreGame[]> {
-    let browser = null;
-    // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-      // running on the Vercel platform.
-      const chromium = require('@sparticuz/chromium-min')
-      const puppeteer = require('puppeteer-core')
-      browser = await puppeteer.launch({
-        args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(
-          `https://github.com/Sparticuz/chromium/releases/download/v127.0.0/chromium-v127.0.0-pack.tar`
-        ),
-        headless: chromium.headless,
-        ignoreHTTPSErrors: true,
-      });
-    // } else {
-    //   // running locally.
-    //   const puppeteer = require('puppeteer')
-    //   browser = await puppeteer.launch()
-    // }
-
-    const browserPage = await browser.newPage()
-    await browserPage.goto(`https://www.flashscore.com/handball/europe/${this.getLeague(competition)}/${page}/`)
-    const games = await browserPage.evaluate(() => {
-      return Array.from(document.querySelectorAll(
-        '.event__match.event__match--static.event__match--twoLine',
-      )).map((game) => {
-        return {
-          id: game?.id.replace("g_7_", ""),
-          dateTime: game?.querySelector('.event__time')?.textContent?.replace("FRO", ""),
-          homeTeam: game?.querySelector('.event__participant--home')?.textContent,
-          awayTeam: game?.querySelector('.event__participant--away')?.textContent,
-          homeScore: Number(game.querySelector('.event__score--home')?.textContent?.replace("-", "0")),
-          awayScore: Number(game.querySelector('.event__score--away')?.textContent?.replace("-", "0")),
-        } as FlashScoreGame
-      })
-    })
-    await browserPage.close()
-    await browser.close()
-    return games
+    const browser = await this.getBrowser()
+    try {
+      const browserPage = await browser.newPage()
+      try {
+        await browserPage.goto(`https://www.flashscore.com/handball/europe/${this.getLeague(competition)}/${page}/`)
+        return await browserPage.evaluate(() => {
+          return Array.from(document.querySelectorAll(
+            '.event__match.event__match--static.event__match--twoLine',
+          )).map((game) => {
+            return {
+              id: game?.id.replace('g_7_', ''),
+              dateTime: game?.querySelector('.event__time')?.textContent?.replace('FRO', ''),
+              homeTeam: game?.querySelector('.event__participant--home')?.textContent,
+              awayTeam: game?.querySelector('.event__participant--away')?.textContent,
+              homeScore: Number(game.querySelector('.event__score--home')?.textContent?.replace('-', '0')),
+              awayScore: Number(game.querySelector('.event__score--away')?.textContent?.replace('-', '0')),
+            } as FlashScoreGame
+          })
+        })
+      } finally {
+        await browserPage.close()
+      }
+    } finally {
+      await browser.close()
+    }
   }
 }
 
